7d67fb463768906b14aa0eec45efe3649c462875
ButakovaLudmila <ludmila.butakova@gmail.com>
2024-05-14 02:31:17 +0300

    12

---
+++ orders/forms.py
@@ -0,0 +1,61 @@
+import re
+from django import forms
+
+
+class CreateOrderForm(forms.Form):
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    phone_number = forms.CharField()
+
+
+    def clean_phone_number(self):
+        data = self.cleaned_data['phone_number']
+
+        if not data.isdigit():
+            raise forms.ValidationError("Номер телефона должен содержать только цифры")
+        
+        pattern = re.compile(r'^\d{10}$')
+        if not pattern.match(data):
+            raise forms.ValidationError("Неверный формат номера")
+
+        return data
+
+
+
+    # first_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите ваше имя",
+    #         }
+    #     )
+    # )
+
+    # last_name = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Введите вашу фамилию",
+    #         }
+    #     )
+    # )
+
+    # phone_number = forms.CharField(
+    #     widget=forms.TextInput(
+    #         attrs={
+    #             "class": "form-control",
+    #             "placeholder": "Номер телефона",
+    #         }
+    #     )
+    # )
+
+
+    # payment_on_get = forms.ChoiceField(
+    #     widget=forms.RadioSelect(),
+    #     choices=[
+    #         ("0", 'False'),
+    #         ("1", 'True'),
+    #     ],
+    #     initial="card",
+    # )
