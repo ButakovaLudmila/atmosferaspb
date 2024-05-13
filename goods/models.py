5d4325830ac83c7b82ba6395a553c912e1e1706f
ButakovaLudmila <ludmila.butakova@gmail.com>
2024-05-13 20:56:21 +0300

    8

--- goods/models.py
+++ goods/models.py
@@ -66,5 +66,4 @@ class Products(models.Model):
     def sell_price(self):
         if self.discount:
             return round(self.price - self.price*self.discount/100, 2)
-        
         return self.price
